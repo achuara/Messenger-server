1e04fcabb292031986a982099878b4710a9f59fa
suman kumar <43146870+achuara@users.noreply.github.com>
2024-01-09 21:12:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://indianmessenger.vercel.app",
+    origin: "https://chat-client-gilt-gamma.vercel.app",
     // credentials: true,
   },
 });
